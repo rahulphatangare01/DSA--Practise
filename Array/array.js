964e327f071e289ba07e0f1a64426dc2c9f40b1e
Rahul phatangare <rahulphatangare01@gmail.com>
2023-10-19 00:55:29 +0530

    start for Array method

---
+++ Array/array.js
@@ -0,0 +1,10 @@
+//  Array methods  
+
+//  1. Array length 
+
+const array = ["Rahul", 1, true, 2, "Amol" , undefined, null ]
+
+console.log(`Array length ${array.length}`);
+
+const content = ["Rahul","Amol","Sagar","Shivam"];
+document.getElementById("demo").innerHTML = content.toString();
